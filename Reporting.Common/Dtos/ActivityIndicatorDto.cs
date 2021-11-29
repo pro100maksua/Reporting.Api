3a21ea9bb84002b654c14b9bd40b8d6e16b7ba89
MaksymKoloda <maksim741@ukr.net>
2021-11-29 17:31:49 +0200

    Implement activity indicators

--- Reporting.Common/Dtos/ActivityIndicatorDto.cs
+++ Reporting.Common/Dtos/ActivityIndicatorDto.cs
@@ -3,11 +3,51 @@
     public class ActivityIndicatorDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
+        public int Year { get; set; }
 
-        public int TypeId { get; set; }
-        public string TypeName { get; set; }
-        public int TypeValue { get; set; }
+        public int ScientificPedagogicalWorkersCount { get; set; }
+
+        public int FullTimeWorkersCount { get; set; }
+        public int FullTimeDoctorOfScienceWorkersCount { get; set; }
+        public int FullTimeCandidatesOfScienceWorkersCount { get; set; }
+        public int FullTimeNoDegreeWorkersCount { get; set; }
+
+        public int PartTimeWorkersCount { get; set; }
+        public int PartTimeDoctorOfScienceWorkersCount { get; set; }
+        public int PartTimeCandidatesOfScienceWorkersCount { get; set; }
+        public int PartTimeNoDegreeWorkersCount { get; set; }
+
+        public int ScientificActivityWorkersCount { get; set; }
+        public int ScientificActivityDoctorOfScienceWorkersCount { get; set; }
+        public int ScientificActivityCandidatesOfScienceWorkersCount { get; set; }
+        public int ScientificActivityNoDegreeWorkersCount { get; set; }
+
+        public int DoctoralStudentsCount { get; set; }
+        public int GraduateStudentsCount { get; set; }
+        public int GraduateStudentsWithBreakFromProductionCount { get; set; }
+
+        public int DefendedCandidateDissertationsCount { get; set; }
+        public int DefendedDoctoralDissertationsCount { get; set; }
+
+        public double StateBudgetFundFinancing { get; set; }
+        public int StateBudgetFundNumberOfWorks { get; set; }
+
+        public double AtExpenseOfCustomersFinancing { get; set; }
+        public int AtExpenseOfCustomersNumberOfWorks { get; set; }
+
+        public double InternationalFundsFinancing { get; set; }
+        public int InternationalFundsNumberOfWorks { get; set; }
+
+        public int CompletedWorksCount { get; set; }
+
+        public int DevelopmentResultsInProductionCount { get; set; }
+        public int DevelopmentResultsInLearningProcessCount { get; set; }
+
+        public int ApplicationsForSecurityDocumentsCount { get; set; }
+        public int ReceivedSecurityDocumentsCount { get; set; }
+
+        public int InventorsCount { get; set; }
+
+        public int ConferencesSeminarsRoundTablesCount { get; set; }
     }
 }
